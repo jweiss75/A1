32aa851eeefe0bec73736778378adaa758ab3863
jweiss <jonaes.mail@gmail.com>
2018-06-13 23:31:42 +0000

    comit

--- src/main/java/scraping/XMLMonster.java
+++ src/main/java/scraping/XMLMonster.java
@@ -12,18 +12,29 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.w3c.dom.NamedNodeMap;
 
 // El sueño de la razón produce monstruos
 
 public class XMLMonster {
-
+    String entidad;
+    String anualidad;
     String tagname    = "*";
     String sourcefile = "XBRL_Files/2016025878.xbrl";
 
+    // Acciona 2010026427
+    // PHM 2016025878
     public static void main(String[] args) throws Exception {
         System.out.println("-------------------El sueño de la razón produce monstruos-----------------------");
+
+
         XMLMonster monster = new XMLMonster();
+        System.out.println("Murcielagos para:" + monster.sourcefile);
+        monster.setTagName("xbrli:context ");
+        monster.searchTags();
+        monster.setTagName("xbrli:entity");
+        monster.searchTags();
         monster.setTagName("ipp-gen:BalanceIndividual");
         monster.searchTags();
         monster.setTagName("ipp-gen:CuentaPerdidasGananciasIndividual");
@@ -32,38 +43,83 @@ public class XMLMonster {
         monster.searchTags();
         monster.setTagName("ipp-gen:CuentaPerdidasGananciasConsolidado");
         monster.searchTags();
-        
-        
+
+
     }
 
-    
-    public static NodeList  getAllChildren(Element e) {
-	       
-	      NodeList allElements = e.getChildNodes();
-	      for (int i = 0; i < allElements.getLength(); i++) {
-	             if  (allElements.item(i).getTextContent().trim().length()!=0) {
-                 System.out.print(" --->Nodo: "+allElements.item(i).getNodeName());
-                 System.out.print(" --->ref: "+allElements.item(i).getAttributes().getNamedItem("contextRef").getNodeValue());
-                 System.out.println(" ------->Valor: "+allElements.item(i).getTextContent().trim());
+
+
+    private String detectaAnualidad(String referencia) throws Exception {
+        String valor = "";
+        List lista = new ArrayList(); // Aqu guardaremos el array.
+        XMLMonster monstruito = new XMLMonster();
+        monstruito.setTagName("xbrli:context");
+        List<Element> listaElementosContexto = monstruito.searchTags();
+
+       for (Element contexto : listaElementosContexto) {
+
+            String id = monstruito.getId(contexto);
+            if (id == referencia) {
+
+                NodeList contextincludedtags = monstruito.getAllChildren(contexto);
+                for (int i = 0; i < contextincludedtags.getLength(); i++) {
+                    System.out.println("----------------------------> AÑO_>>>>> " + contextincludedtags.item(i).getNodeName());
+
+                }
+            }
+        } 
+
+
+        return valor;
+    }
+
+
+    public NodeList getAllChildren(Element e) {
+        NodeList allElements = e.getChildNodes();
+
+
+        for (int i = 0; i < allElements.getLength(); i++) {
+            try {
+
+                if (allElements.item(i).getTextContent().trim().length() != 0) {
+                    System.out.print(" --->Nodo: " + allElements.item(i).getNodeName());
+                    System.out.print(" --->ref: " + allElements.item(i).getAttributes().getNamedItem("contextRef").getNodeValue());
+                    System.out.println(" ------->Valor: " + allElements.item(i).getTextContent().trim());
+                    
+                }
+
+            } catch (Exception ex) {
+
+                System.out.println(">>>> MONSTER genero error:" + ex.getMessage());
+                System.out.println(" ------->Valor: " + allElements.item(i).getTextContent().trim());
+                if (allElements.item(i).getNodeName() == "xbrli:identifier") {
+                    this.entidad = allElements.item(i).getTextContent().trim();
+                    System.out.println("Se detecto la entidad: " + entidad);
                 }
             }
 
-	      
-	      
-	       
-	       return allElements; 
-      }
-    
-    
-     public static  String getContextRef(Element e) {
-	       
-	       String href = e.getAttribute("contextRef");
-	       
-	       return href; 
-      }
+        }
 
 
-    
+        return allElements;
+    }
+
+
+    public static String getContextRef(Element e) {
+
+        String href = e.getAttribute("contextRef");
+
+        return href;
+    }
+
+    public static String getId(Element e) {
+
+        String href = e.getAttribute("id");
+
+        return href;
+    }
+
+
     public List<Element> searchTags() throws Exception {
         List<Element> listadetags = new ArrayList<Element>();
 
@@ -79,11 +135,11 @@ public class XMLMonster {
 
             for (int i = 0; i < entries.getLength(); i++) {
                 Element element = (Element)entries.item(i);
-                
+
                 String nombre = element.getNodeName();
                 NamedNodeMap schema = element.getAttributes();
-        
-                
+
+
                 if (nombre.toLowerCase().indexOf(this.tagname.toLowerCase()) > -1 || this.tagname == "*") {
                     System.out.println("ELEMENTO: " + element.getTagName());
                     System.out.println("TIPO ESQUEMA: " + element.getSchemaTypeInfo());
@@ -91,12 +147,10 @@ public class XMLMonster {
                     System.out.println("URI: " + element.getNodeName());
                     System.out.println("ElementRef: " + getContextRef(element));
                     // Nodes 
-                    
-                    NodeList nodos =  getAllChildren(element);
-                    
-                    
-                    
-                    
+
+                    NodeList nodos = getAllChildren(element);
+
+
                     listadetags.add(element);
                     System.out.println("____________________________");
                 }
