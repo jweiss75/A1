15d1ea625734b4429eee5f16b0877b95a1c24883
jweiss <jonaes.mail@gmail.com>
2018-06-26 17:31:43 +0000

    Comit con elección de empresas por lista

--- src/main/java/PHM.java
+++ src/main/java/PHM.java
@@ -8,9 +8,20 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.net.MalformedURLException;
 import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.ArrayList;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.NamedNodeMap;
 // test de capacidad.
 
 public class PHM {
@@ -24,21 +35,39 @@ public class PHM {
         System.out.println("------------------------------->" + navigate.getRoute());
         try {
             // navigate.submittingForm("VIDRALA");
-            navigate.printCompanies();
-            System.out.println("------------------------------->");
-            navigate.submittingForm("BANCO SANTANDER", navigate.getRoute());
-
-            // unzipfile
-
+            // navigate.printCompanies();
+            // System.out.println("------------------------------->");
             UnzipFile unzip = new UnzipFile();
             unzip.setRoute("XBRL_Files/");
-            unzip.setFileZip("BANCO SANTANDER.zip");
+            
+            List<String> listacotizadas= readData(); 
+            
+            for (String cotizada:listacotizadas){
+               System.out.println(cotizada);   
+               navigate.submittingForm(cotizada, navigate.getRoute());
+               unzip.setFileZip(cotizada+".zip");
+               serialBD();  
+            }
+            
+            
+            
+           /* navigate.submittingForm("FAES", navigate.getRoute());
+            unzip.setFileZip("FAES.zip");
+            unzip.unzip();
+            navigate.submittingForm("PHARMA MAR", navigate.getRoute());
+            unzip.setFileZip("PHARMA MAR.zip");
+            unzip.unzip();     
+            navigate.submittingForm("ALMIRALL", navigate.getRoute());
+            unzip.setFileZip("ALMIRALL.zip");
             unzip.unzip();
 
             // go acreoos de directory of files a use xml for database 
             unzip.serialUnzip();
-
-            serialBD();
+           
+            serialBD();*/
+            
+            
+            
 
 
         } catch (Exception e) {
@@ -49,12 +78,16 @@ public class PHM {
     }
 
     public static void serialBD() throws IOException {
+try
 
+                    {
         System.out.print("Iniciando generación de la base de datos");
 
         File dir = new File("XBRL_Files/");
         String[] ficheros = dir.list();
         DBManage inserciondb = new DBManage();
+        Connection con = inserciondb.conection();
+        XMLHorror monster = new XMLHorror();
         for (String fichero : ficheros) {
             
             
@@ -68,37 +101,33 @@ public class PHM {
                     
                     System.out.println("Se procede a incluir datos en la base de datos");
                     System.out.print("Procesando " + fichero);
-                    XMLMonster monster = new XMLMonster();
                     
-                    monster.setCon(inserciondb.conection());
+                    
+                    
                     monster.setSourceFile("XBRL_Files/" + fichero);
                     monster.setTagName("LegalNameValue");
-
-                    try
-
-                    {
-                        monster.searchTags();
-                        monster.setTagName("context");
-                        XMLMummy mummy = new XMLMummy();
-                        mummy.setListaElemento(monster.searchTags());
-                        monster.setMatrizRefYear(mummy.anualidadList());
-                        System.out.println(mummy.anualidadList());
-                        monster.setMatrizRefYear(mummy.getRefYear());
-                        monster.setTagName("BalanceConsolidado");
-                        monster.searchTags();
-                        monster.closeCon();
-
-                    } catch (Exception e) {
-                        
-                        e.printStackTrace();
-                    }
+                    monster.setConnection();
+                    NodeList documento = monster.nodosDocumeto();
+                    monster.refyear = monster.anualidadList(documento);
+                    monster.entidad= monster.BuscaEntidad(documento, "Entity" );
+                    monster.searchTags(documento, "BalanceConsolidado" );
+                    monster.searchTags(documento, "CuentaPerdidasGananciasConsolidada" );
+                    monster.closeCon();
+                    
+                    
+                    
 
                 }
 
 
             }
+            
+           con.close();
         }
-
+} catch (Exception e) {
+                        
+                        e.printStackTrace();
+                    }
 
     }
 
@@ -108,4 +137,25 @@ public class PHM {
             return dir.getName().endsWith(extension);
         }
     }
+
+
+/////////////////////////// LISTA DE COTIZADAS /////////////////////////////
+
+public static List<String> readData() throws IOException { 
+    int count = 0;
+    String file = "ListedCompanies/IBEX.csv";
+    List<String> content = new ArrayList<>();
+    try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            content.add(line);
+        }
+    } catch (FileNotFoundException e) {
+      //Some error logging
+    }
+    return content;
+}
+
+
+
 }
